ae17b01e5455343b306ecc39c8c67523f1d518f9
Ben Cessa <ben@pixative.com>
2020-07-16 11:59:07 -0400

    Scale QR code

--- utils.go
+++ utils.go
@@ -4,13 +4,18 @@ import (
 	"bytes"
 	"image/png"
 
+	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/qr"
 )
 
-// Generates a new QR code as a PNG image
+// Generates a new QR code as a 250x250 PNG image
 func QRCode(data string) ([]byte, error) {
 	// Generate code
 	code, err := qr.Encode(data, qr.M, qr.Auto)
+	if err != nil {
+		return nil, err
+	}
+	code, err = barcode.Scale(code, 250, 250)
 	if err != nil {
 		return nil, err
 	}
